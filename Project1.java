392c0c078f3311a8383f30647fe423bd6b988744
LAbeast021 <kiarashhesampour@gmail.com>
2023-06-11 20:40:05 -0700

    initating project 1

--- Project1.java
+++ Project1.java
@@ -5,28 +5,27 @@ import java.io.*;
 import java.util.*;
 
 public class Project1 {
+    PrintStream prt = System.out;
+
+    int g[][], visit[], nodes, edges, max = 9999;
 
 
 
     public static void main(String[] args) throws Exception{
-		int cnt = args.length; // get no. of atguments
+		int cnt = args.length;
 		String fn;
 		if (cnt < 1){
 		    System.out.printf("\n\tOOOPS Invalid No. of arguments!" +
 			"\n\tTO Execute: java xxxxxH9 inputfilename");
 			return;
-		} // end if	
+		}
 		
-		// get input file name
 		fn = args[0];
 
-		// create an instance of xxxxxH9 class
 		Project1 g = new Project1();
 		
-		// call process method
 		g.process(fn); 
-			
-		//MAKE SURE TO WRITE YOUR NAME IN NEXT LINE		
+				
 		System.out.printf("\n\tAuthor: K. Hesampour Date: " + java.time.LocalDate.now()); 
-	}	// end main
+	}	
 }
