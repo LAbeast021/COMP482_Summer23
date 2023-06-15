52f675f8eb673d0512230bb8087d4367a7357429
LAbeast021 <kiarashhesampour@gmail.com>
2023-06-15 13:27:03 -0700

    p2 started

--- Project2.java
+++ Project2.java
@@ -4,28 +4,18 @@
 import java.io.*;
 import java.util.*;
 public class Project2 {
+    PrintStream prt = System.out;
 
 
     private void process(String fn) { 
-		int i, j;	  
+		int i, j , people;	  
 		try{ 
 			Scanner inf = new Scanner(new File(fn)); 
-			nodes = inf.nextInt();
-			g = new int[nodes+1][nodes+1];
-			for (i = 1; i <= nodes; i++){
-				for (j = 1; j <= nodes; j++){
-					g[i][j]= inf.nextInt();
-                }
-            }	
+			people = inf.nextInt();
+
 			inf.close();
 		}catch(Exception e){prt.printf("\nI/O Error %s", e );}
-        bfs();
-        if(pathCounter == 0) {
-            prt.printf("There were no paths found " );
-        }
-        else {
-            prt.printf("There were %2d Paths" , pathCounter);
-        }
+
 	}  // end process method
     public static void main(String[] args) throws Exception{
 		String fn;
