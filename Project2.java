a9e1848218f82c4cf03dbe201b4dfad7ec940332
LAbeast021 <kiarashhesampour@gmail.com>
2023-06-15 13:16:32 -0700

    p2 started

--- Project2.java
+++ Project2.java
@@ -1,5 +1,15 @@
 // Kiarash Hesampour
 // Project #2
 public class Project2 {
+
+
+
+    public static void main(String[] args) throws Exception{
+		String fn;
+		fn = "input.txt";
+		Project2 P = new Project2();
+		P.process(fn); 	
+		System.out.printf("\nAuthor: K. Hesampour Date: " + java.time.LocalDate.now()); 
+	}	
     
 }
