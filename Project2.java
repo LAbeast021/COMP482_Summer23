537a5d3304db868aaa156ce596ca01878e87d82c
LAbeast021 <kiarashhesampour@gmail.com>
2023-06-15 13:16:50 -0700

    p2 started

--- Project2.java
+++ Project2.java
@@ -3,7 +3,27 @@
 public class Project2 {
 
 
-
+    private void process(String fn) { 
+		int i, j;	  
+		try{ 
+			Scanner inf = new Scanner(new File(fn)); 
+			nodes = inf.nextInt();
+			g = new int[nodes+1][nodes+1];
+			for (i = 1; i <= nodes; i++){
+				for (j = 1; j <= nodes; j++){
+					g[i][j]= inf.nextInt();
+                }
+            }	
+			inf.close();
+		}catch(Exception e){prt.printf("\nI/O Error %s", e );}
+        bfs();
+        if(pathCounter == 0) {
+            prt.printf("There were no paths found " );
+        }
+        else {
+            prt.printf("There were %2d Paths" , pathCounter);
+        }
+	}  // end process method
     public static void main(String[] args) throws Exception{
 		String fn;
 		fn = "input.txt";
