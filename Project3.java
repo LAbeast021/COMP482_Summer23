25bf26fe5d38183b80975cd8e9eb46859f810329
LAbeast021 <kiarashhesampour@gmail.com>
2023-06-21 13:28:03 -0700

    p3 setup

--- Project3.java
+++ Project3.java
@@ -1,5 +1,11 @@
 // Kiarash Hesampour
 // Project #3
 public class Project3 {
-    
+    public static void main(String[] args) throws Exception{
+		String fn;
+		fn = "input.txt";
+		Project2 P = new Project2();
+		// P.process(fn); 	
+		System.out.printf("\nAuthor: K. Hesampour Date: " + java.time.LocalDate.now()); 
+	}	
 }
