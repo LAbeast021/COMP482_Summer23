29fbace0e272e944a5e2b5fff27fb6afd280d45c
LAbeast021 <kiarashhesampour@gmail.com>
2023-06-26 13:40:30 -0700

    cont p3

--- Project3.java
+++ Project3.java
@@ -1,6 +1,16 @@
 // Kiarash Hesampour
 // Project #3
 public class Project3 {
+
+
+    private void process(String fn) { 
+		int i, j , people;	  
+		try{ 
+			Scanner inf = new Scanner(new File(fn)); 
+			
+		}catch(Exception e){prt.printf("\nI/O Error %s", e );}
+
+	}  // end process method
     public static void main(String[] args) throws Exception{
 		String fn;
 		fn = "input.txt";
