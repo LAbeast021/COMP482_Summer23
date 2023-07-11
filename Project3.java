9be40256a223189509740a0e3fee42c45d480ca4
LAbeast021 <kiarashhesampour@gmail.com>
2023-07-10 22:10:55 -0700

    pr3 almost done

--- Project3.java
+++ Project3.java
@@ -1,12 +1,6 @@
 // Kiarash Hesampour
 // Project #3
 
-// import java.io.BufferedReader;
-// import java.io.File;
-// import java.io.FileReader;
-// import java.io.IOException;
-// import java.util.Scanner;
-
 import java.io.*;
 import java.util.*;
 
