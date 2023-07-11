2374d6dfe3ae231d20a4277c44b7d77054839900
LAbeast021 <kiarashhesampour@gmail.com>
2023-07-10 23:37:02 -0700

    prt 3 done

--- Project3.java
+++ Project3.java
@@ -8,47 +8,46 @@ public class Project3 {
 	PrintStream prt = System.out;	
 
 	private void process(String fn) { 
-		try {
-            BufferedReader inpFile = new BufferedReader(new FileReader(fn));
-			
-            String line = inpFile.readLine();
-            int inputLength = Integer.parseInt(line), counter0 , counter1 , counter2 ;
-            String[] message = new String[inputLength];
+		
+            try (BufferedReader inpFile = new BufferedReader(new FileReader(fn))) {
+				String line = inpFile.readLine();
+				int inputLength = Integer.parseInt(line) , i , j , k;
+				
+				String[] arr = new String[inputLength];
 
-            for ( counter0 = 0; counter0 < inputLength; counter0++) {
-                line = inpFile.readLine();
-                line = line.substring(2);
-                message[counter0] = line;
-            }
+				for ( i = 0; i < inputLength; i++) {
+				    line = inpFile.readLine();
+				    line = line.substring(2);
+				    arr[i] = line;
+				}
 
-            String massage = inpFile.readLine();
-            int[] numOfways = new int[massage.length() + 1];
-            numOfways[0] = 1;
+				String massage = inpFile.readLine();
+				int[] numOfways = new int[massage.length() + 1];
+				numOfways[0] = 1;
 
-            for ( counter0 = 0; counter0 < massage.length(); counter0++) {
-                numOfways[counter0 + 1] = 0;
-            }
+				for ( i = 0; i < massage.length(); i++) {
+				    numOfways[i + 1] = 0;
+				}
 
-            for ( counter0 = 0; counter0 < massage.length(); counter0++) {
-                String tempMassage = massage;
-                tempMassage = tempMassage.substring(0, counter0 + 1);
+				for ( i = 0; i < massage.length(); i++) {
+				    String tempMassage = massage;
+				    tempMassage = tempMassage.substring(0, i + 1);
 
-                for ( counter1 = 0; counter1 <= tempMassage.length(); counter1++) {
-                    String tempTempMassage = tempMassage;
-                    tempTempMassage = tempTempMassage.substring(tempMassage.length() - counter1);
+				    for ( j = 0; j <= tempMassage.length(); j++) {
+				        String tempTempMassage = tempMassage;
+				        tempTempMassage = tempTempMassage.substring(tempMassage.length() - j);
 
-                    for ( counter2 = 0; counter2 < inputLength; counter2++) {
-                        if (tempTempMassage.equals(message[counter2])) {
-                            numOfways[counter0 + 1] += numOfways[counter0 + 1 - tempTempMassage.length()];
-                        }
-                    }
-                }
-            }
+				        for ( k = 0;  k < inputLength;  k++) {
+				            if (tempTempMassage.equals(arr[ k])) {
+				                numOfways[i + 1] += numOfways[i + 1 - tempTempMassage.length()];
+				            }
+				        }
+				    }
+				}
 
-            inpFile.close();
-            prt.println(numOfways[massage.length()]);
-        } 
-		catch(Exception e){prt.printf("\nI/O Error %s", e );}
+				inpFile.close();
+				prt.println(numOfways[massage.length()]);
+        } catch(Exception e){prt.printf("\nI/O Error %s", e );}
 
 	}  // end process method
 
